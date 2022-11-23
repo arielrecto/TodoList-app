99f76296ca65e0721508ac1406c6084de7a31824
arielrecto <arielrecto29@gmail.com>
2022-11-23 21:16:35 +0800

    mysecond commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,11 @@
                     </svg>
                 </div>
 
+
+
+                <div class="flex">
+                    <h1>napaka gwapo ko talaga  ARIEL RECTO POGI</h1>
+                </div>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
