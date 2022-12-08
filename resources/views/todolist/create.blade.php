6f5453046b7dd006fc9df1585a0386f5f494bc63
jennefermorabor <jennifer.morabor@cvsu.edu.ph>
2022-12-09 00:21:37 +0800

    Edittt

--- resources/views/todolist/create.blade.php
+++ resources/views/todolist/create.blade.php
@@ -1,26 +1,36 @@
 <x-layouts.app>
 
-eto ang create view na html files
+    <div class="min-h-screen bg-blue-100 ">
+        @if ($errors->any())
+            {!! implode('', $errors->all('<div>:message</div>')) !!}
+        @endif
 
-@if($errors->any())
-    {!! implode('', $errors->all('<div>:message</div>')) !!}
-@endif
+        @if (Session::has('message'))
+            <p>{{ Session::get('message') }}</p>
+        @endif
 
-@if(Session::has('message'))
-<p>{{ Session::get('message') }}</p>
-@endif
+        <form action="{{ route('store') }}" method="post">
+            <div class="flex justify-center">
+                <div class="flex flex-col w-1/2 space-y-4 mt-20 rounded-lg bg-gray-100 p-5">
+                    <div class="flex justify-center text-2xl font-bold uppercase">
+                        <label for="">Title</label>
+                    </div>
+                    <input type="text" name="title">
+                    <label for="">description</label>
+                    <input type="text" name="description">
+                    <button class="btn btn-success">Submit</button>
 
-<form action="{{route('store')}}" method="post">
+                </div>
+            </div>
 
-    @csrf
-    <label for="">Title</label>
-    <input type="text" name="title">
-    <label for="">description</label>
-    <input type="text" name="description">
-    <button>Submit</button>
+            @csrf
 
-   </form>
 
-<a href="{{route('index')}}">go to index</a>
+        </form>
+
+        <a href="{{ route('index') }}">go to index</a>
+    </div>
+
+
 
 </x-layouts.app>
