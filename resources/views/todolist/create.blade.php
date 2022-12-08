58e8ed1948051a8a7b9fcf9089592b9f2f1e43cf
avrillmonticalvo <avrill.monticalvo@cvsu.edu.ph>
2022-12-09 00:21:15 +0800

    create design

--- resources/views/todolist/create.blade.php
+++ resources/views/todolist/create.blade.php
@@ -1,8 +1,7 @@
 <x-layouts.app>
 
-eto ang create view na html files
-
-@if($errors->any())
+<div class="min-h-screen bg-red-50">
+    @if($errors->any())
     {!! implode('', $errors->all('<div>:message</div>')) !!}
 @endif
 
@@ -11,16 +10,22 @@ eto ang create view na html files
 @endif
 
 <form action="{{route('store')}}" method="post">
-
-    @csrf
+    <div class="flex justify-center">
+    <div class="flex flex-col w-1/2 space-y-4 bg-blue-100 p-5 mt-20 rounded-lg">
+    <div class="flex justify-center text-2xl font-bold uppercase">
     <label for="">Title</label>
+</div>
     <input type="text" name="title">
     <label for="">description</label>
     <input type="text" name="description">
-    <button>Submit</button>
+    <button class="btn btn-success">Submit</button>
 
    </form>
-
+</div>
+</div>
 <a href="{{route('index')}}">go to index</a>
+</div>
+
+
 
 </x-layouts.app>
