02b39759f3473f76225742ea8b49b3d9d6e8c73a
avrillmonticalvo <avrill.monticalvo@cvsu.edu.ph>
2022-12-09 00:37:57 +0800

    header

--- resources/views/todolist/create.blade.php
+++ resources/views/todolist/create.blade.php
@@ -4,14 +4,6 @@
     @if($errors->any())
     {!! implode('', $errors->all('<div>:message</div>')) !!}
 @endif
-    <div class="min-h-screen bg-blue-100 ">
-        @if ($errors->any())
-            {!! implode('', $errors->all('<div>:message</div>')) !!}
-        @endif
-
-        @if (Session::has('message'))
-            <p>{{ Session::get('message') }}</p>
-        @endif
 
         <form action="{{ route('store') }}" method="post">
             <div class="flex justify-center">
